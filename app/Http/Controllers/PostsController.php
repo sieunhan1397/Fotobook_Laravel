24fbe0641decf358a93375a66491088d331477a4
Nhan <sieunhan1397@gmail.com>
2020-03-05 00:32:28 +0700

    add laravel idel helper

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 class PostsController extends Controller
 {
     //
     public function create()
     {
         return view('posts.create');
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'another'   =>  '',
+            'caption'   =>  'required',
+            'image'   =>  'required | image'
+        ]);
+        \app\Post::create($data);
+        dd(request()->all());
     }
 }
